d63c1a2ea6e0dc0f7eef895a47afd38fc18c4314
nguyenhoanganu <129148984+nguyenhoanganu@users.noreply.github.com>
2023-04-04 15:53:16 +0700

    Update CTDL&GT_binary_search_1.cpp

--- CTDL&GT_binary_search_1.cpp
+++ CTDL&GT_binary_search_1.cpp
@@ -16,16 +16,3 @@ int binarySearch(int arr[], int l, int r, int x)
   }
   return -1;
 }
- 
-int main(void) 
-{
-  int arr[] = {2, 3, 4, 10, 40};
-  int n = sizeof(arr) / sizeof(arr[0]);
-  int x = 10;
-  int result = binarySearch(arr, 0, n - 1, x);
-  if (result == -1)
-    printf("%d xuat hien tai chi so %d", x, result);
-  else
-    printf("%d xuat hien tai chi so %d", x, result);
-  return 0;
-}
